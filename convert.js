8d282d32b2e79db3f5752038d5519c8736a09167
Mike Feldstein <msfeldstein@gmail.com>
2017-10-29 11:02:11 -0700

    Make output resolution configurable

--- convert.js
+++ convert.js
@@ -8,11 +8,14 @@ const HDR = require('hdr')
 const writeHDR = require('./write-hdr')
 
 const input = process.argv[2]
-console.log(input)
-if (!input) {
-  console.log("Usage: equirect-to-cubemap path/to/equirect.hdr")
+const outputResolution = parseInt(process.argv[3]) || 256
+
+if (!input || input == '-h') {
+  console.log("Usage: equirect-to-cubemap path/to/equirect.hdr [output resolution]")
   return
 }
+
+console.log(`>> Generating maps for ${input} at ${outputResolution}px`)
 
 const hdrLoader = new HDR.loader()
 hdrLoader.on('load', function() {
@@ -22,7 +25,7 @@ hdrLoader.on('load', function() {
   equirectData.width = this.width
   equirectData.height = this.height
   console.log(this.headers)
-  const cubes = EquirectToCubemapFaces(equirectData, 512, {
+  const cubes = EquirectToCubemapFaces(equirectData, outputResolution, {
     elementCount: 3,
     pixelType: Float32Array,
     alpha: false
